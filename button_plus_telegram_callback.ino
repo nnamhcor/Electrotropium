0246eac093c4645d19793ac3197224c81c3c6df5
nnamhcor <rochmann@ukr.net>
2024-08-24 16:32:28 +0300

    button_plus_telegram_callback.ino

--- button_plus_telegram_callback.ino
+++ button_plus_telegram_callback.ino
@@ -1,4 +1,4 @@
-//version 0.1
+#define ver "version 0.4"
 
 #define WIFI_SSID "xxxxxxxxxxxx"
 #define WIFI_PASS "xxxxxxxxxxxx"
@@ -7,41 +7,42 @@
 
 
 #include <FastBot.h>
+
 FastBot bot(BOT_TOKEN);
 
 
-bool butt_flag = 0;
+bool butt_flag = 1;
+//bool last_butt_flag = 0;
 bool butt;
 bool led_flag = 0;
 unsigned long last_press;
 
+uint32_t startUnix;     // save time
+
+
 void setup() {
 
-  pinMode(D2, INPUT);
-  pinMode(D4, OUTPUT);
-
-
-
+  pinMode(4, INPUT);
+  pinMode(2, OUTPUT);
+  pinMode(16, OUTPUT);
+  digitalWrite(16, TRUE);
   connectWiFi();
-
-  // можно сменить токен
-  //bot.setToken(BOT_TOKEN);
 
   // можно сменить размер буфера на (приём, отправку), по умолч. 512, 512
   //bot.setBufferSizes(1024, 512);
 
-  // установить ID чата, чтобы принимать сообщения только из него
-  // узнать ID можно из ручного запроса в браузере
   bot.setChatID(CHAT_ID); // передай "" (пустую строку) чтобы отключить проверку
 
-  // можно указать несколько ID через запятую
   //bot.setChatID("123456,7891011,12131415");
 
   // подключаем функцию-обработчик
   bot.attach(newMsg);
 
   // отправить сообщение в указанный в setChatID
-  bot.sendMessage("Hello, World!");
+  bot.sendMessage("*Hello, world*");
+  bot.sendMessage(ver);
+
+  startUnix = bot.getUnix(); 
 }
 
 // обработчик сообщений
@@ -50,39 +51,51 @@ void newMsg(FB_msg& msg) {
   //Serial.print(msg.username);
   //Serial.print(", ");
   //Serial.println(msg.text);
-
+  
   // выводим всю информацию о сообщении
-  Serial.println(msg.toString());
+  //Serial.println(msg.toString());
 
+  if (msg.unix < startUnix) return; // ignor massenge
 
   if (msg.text == "/status" && led_flag == 1) {
     bot.sendMessage("нема світла дядя", msg.chatID);
+    
   }
 
 
   if (msg.text == "/status" && led_flag == 0) {
     bot.sendMessage("світло на базі", msg.chatID);
     }
-
+  
 }
-
+  
 
 void loop() {
 
-butt = digitalRead(D2); // считать текущее положение кнопки
-
+butt = digitalRead(4); // считать текущее положение кнопки
+  
   if (butt == 1 && butt_flag == 0 && millis() - last_press > 100) {
     butt_flag = 1;
-    Serial.println("Button pressed");
+    //Serial.println("Button pressed");
     bot.sendMessage("Button pressed!");
     led_flag = !led_flag;
-    digitalWrite(D4, led_flag);
+    digitalWrite(2, led_flag);
     last_press = millis();
   }
   if (butt == 0 && butt_flag == 1) {
     butt_flag = 0;
-    Serial.println("Button released");
+    //Serial.println("Button released");
     bot.sendMessage("Button released!");
+    led_flag = !led_flag;
+    digitalWrite(2, led_flag);
+  }
+
+
+  if(WiFi.status() != WL_CONNECTED){             //led conect
+    digitalWrite(16,HIGH);
+  }
+  else{
+    digitalWrite(16,LOW);
   }
 
 
@@ -92,14 +105,15 @@ butt = digitalRead(D2); // считать текущее положение кн
 
 void connectWiFi() {
   delay(2000);
-  Serial.begin(115200);
-  Serial.println();
+  //Serial.begin(115200);
+  //Serial.println();
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
-    if (millis() > 15000) ESP.restart();
+    //Serial.print(".");
+    if (millis() > 15000){
+      ESP.restart();}
   }
-  Serial.println("Connected");
+    //Serial.println("Connected");
 }
